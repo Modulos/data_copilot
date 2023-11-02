1c580234dffb883d770f7e943a31f3c4e8c64f2c
Michael Roethlisberger <michael.roethlisberger@modulos.ai>
2023-11-02 11:38:30 +0100

    fix dependencies

--- data_copilot/backend/config/config.py
+++ data_copilot/backend/config/config.py
@@ -1,7 +1,8 @@
 import os
 from typing import Dict
 
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
